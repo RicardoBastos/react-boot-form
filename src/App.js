7f07cf22571e21f1d73f22f09f1ab23055eb96f1
Ricardo Bastos <ricardo3bastos@gmail.com>
2021-01-06 01:14:46 -0300

    col

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Row, Col } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
@@ -24,91 +24,120 @@ export default function App() {
     <div className="App">
       <Form
         noValidate
-        style={{ width: "300px" }}
+        style={{ width: "90%" }}
         onSubmit={handleSubmit(onSubmit)}
       >
-        <Form.Group controlId="frmNome">
-          <Form.Label>Nome</Form.Label>
-          <Form.Control
-            size="sm"
-            type="text"
-            name="nome"
-            placeholder="Nome"
-            ref={register}
-            isInvalid={!!errors.nome}
-          />
+        <Row>
+          <Col>
+            <Form.Group controlId="frmNome">
+              <Form.Label>Nome</Form.Label>
+              <Form.Control
+                size="sm"
+                type="text"
+                name="nome"
+                placeholder="Nome"
+                ref={register}
+                isInvalid={!!errors.nome}
+              />
 
-          <Form.Control.Feedback type="invalid">
-            {errors.nome?.message}
-          </Form.Control.Feedback>
-        </Form.Group>
+              <Form.Control.Feedback type="invalid">
+                {errors.nome?.message}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="frmFruta">
+              <Form.Label>Escolha uma fruta</Form.Label>
+              <Form.Control
+                as="select"
+                name="fruta"
+                custom
+                size="sm"
+                defaultValue="Selecione"
+                ref={register}
+                isInvalid={!!errors.fruta}
+              >
+                <option value="">Selecione</option>
+                <option value="1">Maça</option>
+                <option value="2">Pera</option>
+                <option value="3">Uva</option>
+              </Form.Control>
+              <Form.Control.Feedback type="invalid">
+                {errors.fruta?.message}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+        </Row>
 
-        <Form.Group controlId="frmFruta">
-          <Form.Label>Escolha uma fruta</Form.Label>
-          <Form.Control
-            as="select"
-            name="fruta"
-            custom
-            size="sm"
-            defaultValue="Selecione"
-            ref={register}
-            isInvalid={!!errors.fruta}
-          >
-            <option value="">Selecione</option>
-            <option value="1">Maça</option>
-            <option value="2">Pera</option>
-            <option value="3">Uva</option>
-          </Form.Control>
-          <Form.Control.Feedback type="invalid">
-            {errors.fruta?.message}
-          </Form.Control.Feedback>
-        </Form.Group>
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Check
+                required
+                name="termo"
+                label="Aceita os termos?"
+                onChange={() => {}}
+                isInvalid={!!errors.termo}
+                feedback={errors.termo?.message}
+                ref={register}
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group>
+              <Form.Check
+                type="switch"
+                id="viajar"
+                name="viajar"
+                label="Deseja viajar esse ano?"
+                ref={register}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
 
-        <Form.Group>
-          <Form.Check
-            required
-            name="termo"
-            label="Aceita os termos?"
-            onChange={() => {}}
-            isInvalid={!!errors.termo}
-            feedback={errors.termo?.message}
-            ref={register}
-          />
-        </Form.Group>
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Label>Pretente estudar esse ano?</Form.Label>
+              <Form.Check
+                custom
+                name="estudar"
+                type="radio"
+                label="Sim"
+                ref={register}
+                id="estudarSim"
+                value="true"
+                isInvalid={!!errors.estudar}
+              />
+              <Form.Check
+                custom
+                id="estudarNao"
+                name="estudar"
+                type="radio"
+                label="Não"
+                value="false"
+                isInvalid={!!errors.estudar}
+                ref={register}
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group>
+              <Form.Label htmlFor="senha">Senha</Form.Label>
+              <Form.Control
+                type="senha"
+                id="senha"
+                aria-describedby="senhaBlock"
+              />
+              <Form.Text id="senhaBlock" muted>
+                Sua senha precisa ter de 5 a 10 caracteres, conter letras e
+                número(s). Não conter espaços, caracter especial ou emoji.
+              </Form.Text>
+            </Form.Group>
+          </Col>
+        </Row>
 
-        <Form.Group>
-          <Form.Check
-            type="switch"
-            id="viajar"
-            name="viajar"
-            label="Deseja viajar esse ano?"
-            ref={register}
-          />
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Pretente estudar esse ano?</Form.Label>
-          <Form.Check
-            custom
-            name="estudar"
-            type="radio"
-            label="Sim"
-            ref={register}
-            id="estudarSim"
-            value="true"
-            isInvalid={!!errors.estudar}
-          />
-          <Form.Check
-            custom
-            id="estudarNao"
-            name="estudar"
-            type="radio"
-            label="Não"
-            value="false"
-            isInvalid={!!errors.estudar}
-            ref={register}
-          />
-        </Form.Group>
         <Button type="submit">Enviar</Button>
       </Form>
     </div>
